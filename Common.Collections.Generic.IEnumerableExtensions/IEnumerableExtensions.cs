78117e5eb3139f0529e42df12f690f5cfeff09dc
news4vip <news4vip@gmail.com>
2013-03-13 18:48:36 +0900

    Create solution

---
+++ Common.Collections.Generic.IEnumerableExtensions/IEnumerableExtensions.cs
@@ -0,0 +1,111 @@
+﻿namespace Common.Collections.Generic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// System.Collections.Generic.IEnumerableの拡張クラス
+    /// </summary>
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// 列挙子の中身をシャッフルする。
+        /// PHPのshuffle(&Array)のパクリ。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="random"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, Random random)
+        {
+            List<T> copy = source.ToList();
+
+            int count = 0;
+            while (0 < (count = copy.Count))
+            {
+                int index = random.Next(count);
+                yield return copy[index];
+                copy.RemoveAt(index);
+            }
+        }
+
+        /// <summary>
+        /// 列挙子の中身をシャッフルする。
+        /// PHPのshuffle(&Array)のパクリ。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+        {
+            return source.Shuffle<T>(new Random());
+        }
+
+        /// <summary>
+        /// 列挙子の先頭からelementsに該当する要素を削除する。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="elements"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> TrimLeft<T>(this IEnumerable<T> source, params T[] elements)
+        {
+            return source.SkipWhile(e => e.Equals(elements));
+        }
+
+        /// <summary>
+        /// 列挙子の後方からelementsに該当する要素を削除する。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="elements"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> TrimRight<T>(this IEnumerable<T> source, params T[] elements)
+        {
+            return source.Reverse() // 反転して
+                .TrimLeft(elements) // 左トリムして
+                .Reverse();         // もう一度反転して並びを戻す
+        }
+
+        /// <summary>
+        /// 列挙子の両端からelementsに該当する要素を削除する。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="elements"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Trim<T>(this IEnumerable<T> source, params T[] elements)
+        {
+            return source.TrimLeft(elements).TrimRight();
+        }
+
+        /// <summary>
+        /// シーケンスに含まれる唯一の要素を返す。
+        /// シーケンスに含まれる要素が複数の場合には指定された条件を満たす唯一の要素を返す。
+        /// たとえばidと名前のペアがある場合に、idから名前を引くなどという使い方をするもの
+        /// だが、あまり使い道はない。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="predicateIfNotSingle"></param>
+        /// <returns></returns>
+        public static T SingleOrPredicatedSingle<T>(this IEnumerable<T> source, Func<T, Boolean> predicateIfNotSingle)
+        {
+            return source.Count() < 1 ? source.Single() : source.Single(predicateIfNotSingle);
+        }
+
+        /// <summary>
+        /// KeyValuePairコレクションをIDictionaryに変換する。
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="collection"></param>
+        /// <returns></returns>
+        public static IDictionary<TKey, TValue> ToDictionary<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> collection)
+        {
+            return collection.ToDictionary(p => p.Key, p => p.Value);
+        }
+    }
+}
---
+++ Common/Text/ModifiedUTF7.cs
@@ -0,0 +1,161 @@
+﻿namespace Common.Text
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// 修正UTF7変換クラス
+    /// IMAP4のメールボックスを扱うのに使う。
+    /// </summary>
+    public static class ModifiedUTF7
+    {
+        public static string ToModifiedUTF7(string str)
+        {
+            var encoded = new StringBuilder();
+            int shiftFrom = -1;
+
+            for (int index = 0; index < str.Length; index++)
+            {
+                char c = str[index];
+
+                if ((0x20 <= c && c <= 0x7e))
+                {
+                    if (0 <= shiftFrom)
+                    {
+                        // string -> modified UTF7
+                        encoded.Append('&');
+                        encoded.Append(ToModifiedBase64(str.Substring(shiftFrom, index - shiftFrom)));
+                        encoded.Append('-');
+
+                        shiftFrom = -1;
+                    }
+
+                    // printable US-ASCII characters
+                    if (c == 0x26)
+                    {
+                        // except for "&"
+                        encoded.Append("&-");
+                    }
+                    else
+                    {
+                        encoded.Append(c);
+                    }
+                }
+                else
+                {
+                    if (shiftFrom == -1)
+                    {
+                        shiftFrom = index;
+                    }
+                }
+            }
+
+            if (0 <= shiftFrom)
+            {
+                // string -> modified UTF7
+                encoded.Append('&');
+                encoded.Append(ToModifiedBase64(str.Substring(shiftFrom)));
+                encoded.Append('-');
+            }
+
+            return encoded.ToString();
+        }
+
+        private static string ToModifiedBase64(string str)
+        {
+            byte[] bytes = Encoding.BigEndianUnicode.GetBytes(str);
+            string base64 = Convert.ToBase64String(bytes).Replace('/', ',');
+            int padding = base64.IndexOf('=');
+
+            if (padding < 0)
+            {
+                return base64;
+            }
+            else
+            {
+                return base64.Substring(0, padding);
+            }
+        }
+
+        public static string FromModifiedUTF7(string str)
+        {
+            if (!str.Contains("&"))
+            {
+                return str;
+            }
+
+            byte[] bytes = Encoding.ASCII.GetBytes(str);
+            var decoded = new StringBuilder();
+
+            for (int index = 0; index < bytes.Length; index++)
+            {
+                // In modified UTF-7, printable US-ASCII characters, except for "&",
+                // represent themselves
+                // "&" is used to shift to modified BASE64
+                if (bytes[index] != 0x26)
+                { // '&'
+                    decoded.Append((char)bytes[index]);
+                    continue;
+                }
+
+                if (bytes.Length <= ++index)
+                {
+                    // incorrect form
+                    throw new FormatException("incorrect form");
+                }
+
+                if (bytes[index] == 0x2d)
+                { // '-'
+                    // The character "&" (0x26) is represented by the two-octet sequence "&-".
+                    decoded.Append('&');
+                    continue;
+                }
+
+                var nonPrintable = new StringBuilder();
+
+                for (; index < bytes.Length; index++)
+                {
+                    if (bytes[index] == 0x2d) // '-'
+                    {
+                        // "-" is used to shift back to US-ASCII
+                        break;
+                    }
+
+                    nonPrintable.Append((char)bytes[index]);
+                }
+
+                // modified UTF7 -> string
+                decoded.Append(FromModifiedBase64(nonPrintable.ToString()));
+            }
+
+            return decoded.ToString();
+        }
+
+        private static string FromModifiedBase64(string str)
+        {
+            byte[] buf = null;
+
+            // "," is used instead of "/"
+            str = str.Replace(',', '/');
+
+            int padding = 4 - str.Length & 3;
+
+            if (padding == 3)
+            {
+                throw new FormatException("incorrect form");
+            }
+            else if (padding == 4)
+            {
+                buf = Convert.FromBase64String(str);
+            }
+            else
+            {
+                buf = Convert.FromBase64String(str + (new string('=', padding)));
+            }
+
+            return Encoding.BigEndianUnicode.GetString(buf);
+        }
+    }
+}
